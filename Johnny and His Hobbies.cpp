da74c05a22362b130c6b6bf29137ed2ad7dd3df2
Harsh Dhiman <43261336+harsh725@users.noreply.github.com>
2020-06-15 23:29:44 +0530

    Add files via upload

---
+++ Johnny and His Hobbies.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+#define eb  emplace_back
+#define loop(a,n) for(ll i=0;i<n;i++)
+#define debug(x)  cout<<#x<<" "<<x<<"\n";
+#define mod 1e9+7
+#define setbits(x)  __builtin_popcountll(x)
+#define digits(a,x)  a=floor(log10(x)) + 1
+
+bool prime(ll s);
+ll power(ll x, ll y) ;
+
+void solve()
+{
+    ll n;
+    cin>>n;
+std::vector<ll> a(n),b(n);    
+    
+    loop(0,n)
+cin>>a[i];
+sort(a.begin(),a.end());
+    
+    for(ll i=1;i<=1024;i++)
+    {
+        for(ll j=0;j<n;j++)
+        {
+            b[j]=a[j]^i;
+        }
+        sort(b.begin(),b.end());
+        if(a==b)
+        {
+            cout<<i<<"\n";
+            return ;
+        }
+    }
+    cout<<"-1\n";
+    
+    
+    
+    
+    
+    
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    ll t;cin>>t;
+    // int t=1;
+    while(t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
+
+
+
+bool prime(ll s)
+{
+    if (s <= 1)  return false;
+    if (s <= 3)  return true;
+    if (s%2 == 0 || s%3 == 0) return false;
+
+    for (ll i=5; i*i<=s; i=i+6)
+        if (s%i == 0 || s%(i+2) == 0)
+           return false;
+
+    return true;
+}
+
+ll power(ll x, ll y) 
+{ 
+    ll temp; 
+    if( y == 0) 
+        return 1; 
+    temp = power(x, y/2); 
+    if (y%2 == 0) 
+        return temp*temp; 
+    else
+        return x*temp*temp; 
+} 
