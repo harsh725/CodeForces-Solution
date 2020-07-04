c08fc77ca308233a14a51fdc4e0b85541bb97d3e
Harsh Dhiman <harsh.dhiman725@gmail.com>
2020-07-04 16:19:19 +0530

    Problem C

---
+++ Educational Codeforces Round 90 (Rated for Div. 2)/C - Pluses and Minuses .cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+#define eb  emplace_back
+#define loop(a,n) for(ll i=a;i<n;i++)
+#define debug(x)  cout<<#x<<" "<<x<<"\n";
+#define mod 1e9+7
+#define setbits(x)  __builtin_popcountll(x)
+#define digits(a,x)  a=floor(log10(x)) + 1
+
+bool prime(ll s);
+ll power(ll x, ll y) ;
+
+bool ispower2(ll n)
+{
+    
+    return n && (!(n&(n-1)));
+}
+void solve()
+{
+    string s;
+    cin>>s;
+    ll n=s.size();
+    std::vector<int> v(n+1);
+    v[0]=0;
+    loop(0,n)
+    if(s[i]=='+')
+    v[i+1]=v[i]+1;
+    else
+    v[i+1]=v[i]-1;
+        
+        ll res=0,x=-1;
+    loop(1,n+1)
+    if(v[i]==x)
+    {
+        res+=i;
+        x--;
+    }
+    res+=n;
+    cout<<res<<endl;
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    ll t;cin>>t;
+    // int t=1;
+    while(t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
+
+
+
+bool prime(ll s)
+{
+    if (s <= 1)  return false;
+    if (s <= 3)  return true;
+    if (s%2 == 0 || s%3 == 0) return false;
+
+    for (ll i=5; i*i<=s; i=i+6)
+        if (s%i == 0 || s%(i+2) == 0)
+           return false;
+
+    return true;
+}
+
+ll power(ll x, ll y) 
+{ 
+    ll temp; 
+    if( y == 0) 
+        return 1; 
+    temp = power(x, y/2); 
+    if (y%2 == 0) 
+        return temp*temp; 
+    else
+        return x*temp*temp; 
+} 
