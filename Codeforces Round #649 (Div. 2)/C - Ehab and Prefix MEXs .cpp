10c5acf6f1cd037bb87a08a8d0f22738399e365d
harsh725 <harsh.dhiman725@gmail.com>
2020-07-24 23:26:43 +0530

    C

---
+++ Codeforces Round #649 (Div. 2)/C - Ehab and Prefix MEXs .cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+#define eb  emplace_back
+#define all(x) x.begin(),x.end()
+#define loop(a,n) for(ll i=a;i<n;i++)
+#define debug(x)  cout<<#x<<" "<<x<<"\n";
+#define mod 1e9+7
+#define setbits(x)  __builtin_popcountll(x)
+#define digits(a,x)  a=floor(log10(x)) + 1
+
+bool prime(ll s);
+ll power(ll x, ll y) ;
+
+bool ispower2(ll n)
+{
+    
+    return n && (!(n&(n-1)));
+}
+void solve()
+{
+    ll n,x;
+    cin>>n;
+    ll a[n];
+    loop(0,n)
+    cin>>a[i];
+    
+    std::unordered_map<ll, bool> m;
+    // debug(m[2]);
+    m[a[n-1]]=1;
+    std::vector<ll> v;
+    // v.emplace_back(a[n-2]);
+    // m[v[0]]=1;
+    ll maxi=n;
+    
+    for(ll i=n-2;i>=0;i--)
+    {
+        
+        
+        if(a[i]!=a[i+1])
+        {
+            v.emplace_back(a[i]);
+            m[a[i]]=1;
+            continue;
+        }
+        while(m[maxi]==1)
+        maxi--;
+        
+        v.emplace_back(maxi);
+        m[maxi]=1;
+        
+        
+    }
+    while(m[maxi]==1)
+        maxi--;
+        
+        v.emplace_back(maxi);
+    for(ll i=n-1;i>=0;i--)
+    cout<<v[i]<<" ";
+    
+    
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    ll t=1;
+    // cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
+
+
+
+bool prime(ll s)
+{
+    if (s <= 1)  return false;
+    if (s <= 3)  return true;
+    if (s%2 == 0 || s%3 == 0) return false;
+
+    for (ll i=5; i*i<=s; i=i+6)
+        if (s%i == 0 || s%(i+2) == 0)
+           return false;
+
+    return true;
+}
+
+ll power(ll x, ll y) 
+{ 
+    ll temp; 
+    if( y == 0) 
+        return 1; 
+    temp = power(x, y/2); 
+    if (y%2 == 0) 
+        return temp*temp; 
+    else
+        return x*temp*temp; 
+} 
