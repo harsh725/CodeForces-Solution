3a94efe43e44ede35648a7b1a126d77023ce695f
harsh725 <harsh.dhiman725@gmail.com>
2020-07-28 18:04:48 +0530

    3rd

---
+++ Codeforces Round #638 (Div. 2)/C - Phoenix and Distribution.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+#define eb  emplace_back
+#define all(x) x.begin(),x.end()
+#define loop(a,n) for(ll i=a;i<n;i++)
+#define debug(x)  cout<<#x<<" "<<x<<"\n";
+#define mod 1e9+7
+#define setbits(x)  __builtin_popcountll(x)
+#define digits(a,x)  a=floor(log10(x)) + 1
+
+bool prime(ll s);
+ll power(ll x, ll y) ;
+
+bool ispower2(ll n)
+{
+    
+    return n && (!(n&(n-1)));
+}
+void solve()
+{
+    ll n,k;cin>>n>>k;
+    string s;cin>>s;
+    sort(all(s));
+    
+    if(s[0]!=s[k-1])
+    {
+        cout<<s[k-1]<<"\n";return ;
+    }
+    cout<<s[0];
+    if(s[k]!=s[n-1])
+    {
+        loop(k,n)
+        cout<<s[i];
+    }
+    else
+    {
+        loop(0,(n-1)/k)
+        cout<<s[n-1];
+    }
+    cout<<endl;
+    
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    ll t=1;
+    cin>>t;
+    ll x=1;
+    while(t--)
+    {
+        // cout<<"Case #"<<x++<<": ";
+        solve();
+    }
+    
+    return 0;
+}
+
+
+
+bool prime(ll s)
+{
+    if (s <= 1)  return false;
+    if (s <= 3)  return true;
+    if (s%2 == 0 || s%3 == 0) return false;
+
+    for (ll i=5; i*i<=s; i=i+6)
+        if (s%i == 0 || s%(i+2) == 0)
+           return false;
+
+    return true;
+}
+
+ll power(ll x, ll y) 
+{ 
+    ll temp; 
+    if( y == 0) 
+        return 1; 
+    temp = power(x, y/2); 
+    if (y%2 == 0) 
+        return temp*temp; 
+    else
+        return x*temp*temp; 
+} 
