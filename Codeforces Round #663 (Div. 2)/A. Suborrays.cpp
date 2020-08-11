ce5bda9195ca9c299eabd33a33c0c17321cb27e9
harsh725 <harsh.dhiman725@gmail.com>
2020-08-11 17:18:46 +0530

    A

---
+++ Codeforces Round #663 (Div. 2)/A. Suborrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;cin>>n;
+	while(n--)
+	{
+	    int x;cin>>x;
+	    for(int i=1;i<=x;i++)
+	    cout<<i<<" ";
+	    
+	    cout<<"\n";
+	}
+	return 0;
+}
