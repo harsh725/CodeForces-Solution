1b1779b35007e74f3d94b908e8b06cd003678650
harsh725 <harsh.dhiman725@gmail.com>
2020-08-21 22:44:27 +0530

    math

---
+++ Codeforces Round #665 (Div. 2)/B - Ternary Sequence .cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+#define eb  emplace_back
+#define all(x) x.begin(),x.end()
+#define loop(a,n) for(ll i=a;i<n;i++)
+#define debug(x)  cout<<#x<<" "<<x<<"\n";
+#define mod 1e9+7
+#define setbits(x)  __builtin_popcountll(x)
+#define digits(a,x)  a=floor(log10(x)) + 1
+
+bool prime(ll s);
+ll power(ll x, ll y) ;
+
+bool ispower2(ll n)
+{
+    
+    return n && (!(n&(n-1)));
+}
+void solve()
+{
+    ll a,b,c,x,y,z;cin>>a>>b>>c>>x>>y>>z;
+    ll sum=0;
+    // c*y max
+    ll ma=min(c,y);
+    sum+=2*ma;
+    c-=ma;y-=ma;
+    z-=c;
+    ll mi=min(b,y);
+    b-=mi;y-=mi;
+    z-=a;
+    if(z>0 and b>0)
+    {
+        sum-=2*min(z,b);
+    }
+    cout<<sum<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    ll t=1;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
+
+
+
+bool prime(ll s)
+{
+    if (s <= 1)  return false;
+    if (s <= 3)  return true;
+    if (s%2 == 0 || s%3 == 0) return false;
+
+    for (ll i=5; i*i<=s; i=i+6)
+        if (s%i == 0 || s%(i+2) == 0)
+           return false;
+
+    return true;
+}
+
+ll power(ll x, ll y) 
+{ 
+    ll temp; 
+    if( y == 0) 
+        return 1; 
+    temp = power(x, y/2); 
+    if (y%2 == 0) 
+        return temp*temp; 
+    else
+        return x*temp*temp; 
+} 
