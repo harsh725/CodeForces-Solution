58ad95cf7e80366f0511d266fe72d7faeb13acff
harsh725 <harsh.dhiman725@gmail.com>
2020-09-12 20:50:40 +0530

    math

---
+++ Codeforces Round #670 (Div. 2)/A. Subset Mex.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+#define eb  emplace_back
+#define all(x) x.begin(),x.end()
+#define loop(a,n) for(ll i=a;i<n;i++)
+#define debug(x)  cout<<#x<<" "<<x<<"\n";
+#define mod 1e9+7
+#define setbits(x)  __builtin_popcountll(x)
+#define digits(a,x)  a=floor(log10(x)) + 1
+#define ss second
+#define ff first
+
+bool prime(ll s);
+ll power(ll x, ll y) ;
+bool cmp(ll a, ll b)
+{
+    return a>b;
+}
+bool ispower2(ll n)
+{
+    
+    return n && (!(n&(n-1)));
+}
+void solve()
+{
+    ll n;
+    cin>>n;
+    ll a[110]={0};
+    
+    loop(0,n)
+    {
+        ll x;cin>>x;
+        a[x]++;
+    }
+    
+    if(a[0]==0)
+    {
+        cout<<"0\n";return;
+    }
+    a[0]--;
+    ll x=1;
+    loop(1,n)
+    {
+        if(a[i]==0)
+        {
+            break;
+        }
+        x++;
+        a[i]--;
+    }
+    
+    if(x==0)
+    {
+        cout<<"0\n";return;
+    }
+    ll k=1;
+    if(a[0]<=0)
+    k=0;
+    else
+    {
+        loop(1,n)
+        {
+            if(a[i]<=0)
+            break;
+            k++;
+        }
+    }
+    cout<<x+k<<"\n";
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    ll t=1;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
+
+
+
+bool prime(ll s)
+{
+    if (s <= 1)  return false;
+    if (s <= 3)  return true;
+    if (s%2 == 0 || s%3 == 0) return false;
+
+    for (ll i=5; i*i<=s; i=i+6)
+        if (s%i == 0 || s%(i+2) == 0)
+           return false;
+
+    return true;
+}
+
+ll power(ll x, ll y) 
+{ 
+    ll temp; 
+    if( y == 0) 
+        return 1; 
+    temp = power(x, y/2); 
+    if (y%2 == 0) 
+        return temp*temp; 
+    else
+        return x*temp*temp; 
+} 
